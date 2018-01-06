f1400c0f2607ba90091f1c96971d5010ae31ff60
Julian Lucas Wohlleber <Julian@Lucaswohlleber.de>
2018-01-06 13:14:58 +0100

    prepared the bigger new planned body
    - prepared the changes discussed with boris
    - started to implement the lightshaft as well as trigger/ankerpoints
    for activating/deactivating different sections
    - build objects for relating variables
    
    future steps:
    - excchange if for switch/case
    - find a way to draw a vector-curve in sketch which will be transferred
    to a non-linear behaviour for the section lightshaft (or probably a
    logarythmic function is enough?
    - build all the functions/cases inside the different sections
    
    ———
    
    all future builds for garden-specific visual behaviours will be
    processed in the branch “light & shadow”

--- JMD-Website/js/garden.js
+++ JMD-Website/js/garden.js
@@ -1,54 +1,111 @@
 //######################SETUP##########################
 
-//VARS parametric
-var contentAbove = 0;
-var gardenSize = 9000;
-var pillarSize = 300; //dimensions of the rects
-const scaleW = 1680; //width for which was designed for
-var fuzzyRadius = 40; //spreading of the light
-var backgroundColor = "rgba(1, 0, 0, 0.3)";
-var shadowSideStripes = "rgba(100,100,100,0.1)";
-var rectsColor = "black";
+//VARS parametric influencing parameters
+var garden = {
+	size: 9000,
+	scaleW: 1680, //width for which was designed for (everything should be scaled down for smaller size - not done yet)
+	// sectionPoints: {
+	// 	a:{
+	// 		[0, 200], //white_1
+	// 		[400, 500], //threequaterwhite_1
+	// 		[700, 900], //halfwhite_1
+	// 		[1100,1150], //quaterwhite_1
+	// 		[1400, 1800], //onpixel_1
+	// 		[2500, 2900], //blackend/start
+	// 		[3300, 3500], //onepixel_2
+	// 		[3600, 3800], //quaterwhite_2
+	// 		[4000, 4500], //halfwhite_2
+	// 		[4700, 4900], //threequaterwhite_2
+	// 		[5100, 5500]
+	// 	}
+	// }
+}
+
+var pillar = {
+	active: true, //determines wheather the pillars
+	size: 300, //sets the size of the pillars
+	color: "black",
+	points: [ //x,y coordinates for the pillars on the canvas
+		{x: 0, y: 0},
+		{x: 170, y: 500},
+		{x: 170, y: 2450},
+		{x: 170, y: 5230},
+		{x: 476, y: 1218},
+		{x: 476, y: 3222},
+		{x: 476, y: 4610},
+		{x: 1088, y: 213},
+		{x: 1088, y: 2736},
+		{x: 1394, y: 1296},
+		{x: 1394, y: 3444},
+	]
+}
+
+var lightShaft = {
+	active: false, //determinates wheather the shaft is drawn
+	x1: 0,
+	x2: 0,
+	y1: 0, //start of the shaft
+	y2: 0, //end of the shaft
+	middle: {
+		width: 1, //size of the shaft
+		color: "white"
+	},
+	border: {
+		width: 1, //size of the surrounding border
+		opacity: 1
+	}
+}
+
+var lightSpot = {
+	active: false, //determines whaether light spot is active (visually)
+	x: 0,
+	y: 0,
+	opacity: 0,
+	width: 400,
+	height: 1000,
+	image: new Image()
+}
+
+var shadow = {
+	active: true,
+	background: "rgba(1, 0, 0, 0.3)", //color of the background
+	color: "black", //not linked with css yet
+	intersections: {
+		color: "rgba(100,100,100,0.1)", //color of the shadow overlays
+		fuzzyness: 40 //spreading of the light, also relates to the lightshaft-size
+	}
+}
+
 
 //BOOLEANS
 var updateCanvas = true;
 
-//VARS GENERIC
+
+//VARS GENERIC (changed during the sketch)
 var windowOffset = 0;
+
 var Mouse = {
 	x: 0,
 	y: 0
 };
-var scaleFactor = window.innerWidth/scaleW
-var lightSpotImage = new Image();
 
-// LINKS
+
+// linking objects to the html
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-// ELEMENTS Scaled for 1680w
+
+// Necessary for storing the points of the pillars
 var segments = [];
 
-//x,y coordinates for the rects on the canvas
-var rectPlaces = [
-	{x: 0, y: 0},
-	{x: 170, y: 500},
-	{x: 170, y: 2450},
-	{x: 170, y: 5230},
-	{x: 476, y: 1218},
-	{x: 476, y: 3222},
-	{x: 476, y: 4610},
-	{x: 1088, y: 213},
-	{x: 1088, y: 2736},
-	{x: 1394, y: 1296},
-	{x: 1394, y: 3444},
-];
 //relations
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
 
 
 //#####################FUNCTIONS#######################
+
 //updating functions:
+
 // Find intersection of RAY & SEGMENT
 function getIntersection(ray,segment){
 
@@ -92,6 +149,7 @@ function getIntersection(ray,segment){
 	};
 }
 
+//calculates the shadow-polygons
 function getSightPolygon(sightX,sightY){
 
 	// Get all unique points
@@ -168,7 +226,7 @@ function getSightPolygon(sightX,sightY){
 
 }
 
-//calculates dots for positioning
+//calculates dots for positioning relating to calc-segments
 function calcShift(dot,x,y){
 	var point = {};
 	point.x = dot.x + x;
@@ -176,10 +234,10 @@ function calcShift(dot,x,y){
 	return point;
 }
 
-// calculates segments dots from x,y values of the rects
+// calculates segments dots from x,y values of the pillars
 function calcSegments(i_min, i_max, rectSize){
 	for(var i = i_min; i<i_max; i++){
-		const dot = jQuery.extend(true, {}, rectPlaces[i]);
+		const dot = jQuery.extend(true, {}, pillar.points[i]);
 		var line = {};
 
 
@@ -204,34 +262,8 @@ function calcSegments(i_min, i_max, rectSize){
 		segments.push(jQuery.extend(true, {}, line));
 	}
 }
-// function calcSegments(){
-// 	for(var i = 0; i<rectPlaces.length; i++){
-// 		const dot = jQuery.extend(true, {}, rectPlaces[i]);
-// 		var line = {};
-//
-//
-// 		//line1
-// 		line.a = calcShift(dot,0,0);
-// 		line.b = calcShift(dot,0,rectSize);
-// 		segments.push(jQuery.extend(true, {}, line));
-//
-// 		//line2
-// 		line.a = calcShift(dot,0,rectSize);
-// 		line.b = calcShift(dot,rectSize,rectSize);
-// 		segments.push(jQuery.extend(true, {}, line));
-//
-// 		// //line3
-// 		line.a = calcShift(dot,rectSize,rectSize);
-// 		line.b = calcShift(dot,rectSize,0);
-// 		segments.push(jQuery.extend(true, {}, line));
-//
-// 		// //line4
-// 		line.a = calcShift(dot,rectSize,0);
-// 		line.b = calcShift(dot,0,0);
-// 		segments.push(jQuery.extend(true, {}, line));
-// 	}
-// }
-//draws a polygon that forms or leaves the shadows free
+
+//draws the shadows
 function drawPolygon(polygon,ctx,fillStyle){
 	ctx.fillStyle = fillStyle;
 	ctx.beginPath();
@@ -246,43 +278,50 @@ function drawPolygon(polygon,ctx,fillStyle){
 //Everything visible in the canvas
 function draw(){
 
-	//define the point of the lightSpotImage
-	var spotLight = {};
-	spotLight.x = canvas.width/2;
-	spotLight.y = (windowOffset/$(document).height())*canvas.height*1.4 - 80
-	spotLight.x = Mouse.x
-	// spotLight.y = Mouse.y
+	scrollLight(0, 200,400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200)
+
+	//set the lightspot to the center of the screen and make it relate to the scrollingposition (obsolete)
+	lightSpot.x = canvas.width/2;
+	lightSpot.y = (windowOffset/$(document).height())*canvas.height*1.4 - 80
+	// lightSpot.x = Mouse.x
+	// lightSpot.y = Mouse.y
 
 	// Clear canvas
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	// Sight Polygons
-	var polygons = [getSightPolygon(spotLight.x,spotLight.y+windowOffset)];
-	for(var angle=0;angle<Math.PI*2; angle+=(Math.PI*2)/10){
-		var dx = Math.sin(angle)*fuzzyRadius;
-		var dy = Math.cos(angle)*fuzzyRadius;
-		polygons.push(getSightPolygon((spotLight.x+dx) ,(spotLight.y+dy+windowOffset)));
-	};
 
-	drawPolygon(polygons[0],ctx,backgroundColor);
+	if(shadow.active){
+		// Sight Polygons
+		var polygons = [getSightPolygon(lightSpot.x,lightSpot.y+windowOffset)];
+		for(var angle=0;angle<Math.PI*2; angle+=(Math.PI*2)/10){
+			var dx = Math.sin(angle)*shadow.intersections.fuzzyness;
+			var dy = Math.cos(angle)*shadow.intersections.fuzzyness;
+			polygons.push(getSightPolygon((lightSpot.x+dx) ,(lightSpot.y+dy+windowOffset)));
+		};
 
-	// DRAW AS A GIANT POLYGON
-	for(var i=1;i<polygons.length;i++){
-		drawPolygon(polygons[i],ctx, shadowSideStripes);
+		drawPolygon(polygons[0],ctx,shadow.background);
+		// DRAW AS A GIANT POLYGON
+		for(var i=1;i<polygons.length;i++){
+			drawPolygon(polygons[i],ctx, shadow.intersections.color);
+		}
 	}
 
-	drawShaft(spotLight.x, spotLight.y);
+	if(lightShaft.active){
+	drawShaft(lightShaft.x1, lightShaft.y1, lightShaft.x2, lightShaft.y2, lightShaft.width);
+	}
 
-	ctx.drawImage(lightSpotImage,spotLight.x-lightSpotImage.width/2,spotLight.y-lightSpotImage.height/2);
+	ctx.drawImage(lightSpot.image,lightSpot.x-lightSpot.image.width/2,lightSpot.y-lightSpot.image.height/2, lightSpot.image.width, lightSpot.image.height);
 
-	for(var i=0;i<rectPlaces.length;i++){
-		ctx.fillStyle = rectsColor
-		var spot = rectPlaces[i];
-		ctx.beginPath();
-		ctx.moveTo(spot.x ,spot.y -windowOffset);
-		ctx.lineTo(spot.x+pillarSize,spot.y -windowOffset);
-		ctx.lineTo(spot.x+pillarSize,spot.y + pillarSize -windowOffset);
-		ctx.lineTo(spot.x,spot.y+pillarSize -windowOffset);
-		ctx.fill();
+	if(pillar.active){
+		for(var i=0;i<pillar.points.length;i++){
+			ctx.fillStyle = pillar.color
+			var spot = pillar.points[i];
+			ctx.beginPath();
+			ctx.moveTo(spot.x ,spot.y -windowOffset);
+			ctx.lineTo(spot.x+pillar.size,spot.y -windowOffset);
+			ctx.lineTo(spot.x+pillar.size,spot.y + pillar.size -windowOffset);
+			ctx.lineTo(spot.x,spot.y+pillar.size -windowOffset);
+			ctx.fill();
+		}
 	}
 }
 
@@ -298,44 +337,82 @@ function drawLoop(){
 
 //everything related to the canvas size
 function resizeCanvas(){
-	var scaleFactor = window.innerWidth/scaleW
+	var scaleFactor = window.innerWidth/garden.scaleW
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	}
+}
 
-// //logarythmic function for sun movement
-// function moveSpotlight(x, radiusHeightRatio){
-// 	radius = radiusHeightRatio * $(document).height()
-// 	centerX = canvas.width/2//-radius/2
-// 	centerY = canvas.height/2
-// }
-
-function drawShaft(x,y){
+//draw shaft line
+function drawShaft(x1,y1, x2, y2, shaftSize){
 	ctx.strokeStyle = "white"
 	ctx.beginPath()
-	ctx.moveTo(x,0);
-	ctx.lineTo(x, y);
-	ctx.lineTo(x, 0);
+	ctx.moveTo(x1,y1);
+	ctx.lineTo(x2, y2);
+	ctx.lineTo(x1, y1);
+	ctx.lineWidth = shaftSize;
 	ctx.stroke();
 }
 
+//a function to map a number from one area to another (S = Source, T = Target)
+function mapArea(x, min_S, max_S, min_T, max_T){
+	var y = (max_T-min_T)*(x-min_S)/(max_S-min_S) + min_T
+	return y
+}
+
+//goes through the stages of one lightloop mapping the states on the scrollingposition. For more information take the relating sketch file from the folder 04_wireframes/02_juli folder
+function scrollLight(fullwhite_1,threequaterwhite_1, halfwhite_1, quaterwhite_1, onpixel_1, blackstart, blackend, onepixel_2, quaterwhite_2, halfwhite_2, threequaterwhite_2, fullwhite_2, loopend){
+// 	var section = ''
+// 	if(loopend<=windowOffset){
+// 		return
+// 	if(fullwhite_2<=windowOffset){
+// 		section = 'fullwhite+'
+// 	}else if (threequaterwhite_2<windowOffset) {
+// 		section = '3/4white+'
+// 	}else if (threequaterwhite_2<windowOffset) {
+// 	section = '1/2white+'
+// }else if (threequaterwhite_2<windowOffset) {
+// 	section = '1/4white+'
+// }else if (threequaterwhite_2<windowOffset) {
+// 	section = '3/4white+'
+// }
+
+	lightShaft.active = (fullwhite_1<=windowOffset && windowOffset<blackstart || blackend<windowOffset && windowOffset<=fullwhite_2); //activates/deactivates the drawing of the lightshaft depending on the scrollposition
+	lightSpot.active = (halfwhite_1< windowOffset && windowOffset<onpixel_1 || onepixel_2<windowOffset && windowOffset<halfwhite_2); //activates/deactivates the lightSpotImage
+	shadow.active = (quaterwhite_1<windowOffset && windowOffset<quaterwhite_2); //activates/deactivates shadows
+	pillar.active = (quaterwhite_1<windowOffset && windowOffset<quaterwhite_2); //activates/deactivates pillars
+
+
+	// switch (windowOffset){
+	// 	case
+	// }
+	if(fullwhite_1<=windowOffset<onpixel_1){
+		lightShaft.x1 = window.innerWidth/2
+		lightShaft.x2 = lightShaft.x1
+		lightShaft.y1 = 0;
+		lightShaft.y2 = window.innerHeight;
+		// if(windowOffset<threequaterwhite_1){
+		lightShaft.width = mapArea(windowOffset, fullwhite_1, onpixel_1, window.innerWidth, 1);
+	// 	console.log(lightShaft.width);
+	// }
+	}
+}
 
 
 //######################EVENTS##########################
 
 //recalculates segments
-calcSegments(0,1, gardenSize)
-calcSegments(1, rectPlaces.length-1, pillarSize);
-// calcSegments();
+calcSegments(0,1, garden.size)
+calcSegments(1, pillar.points.length-1, pillar.size);
+
 // resize the canvas to fill browser window dynamically
 window.addEventListener('resize', resizeCanvas, false);
 
 //everything happening on pageload
 window.onload = function(){
-	lightSpotImage.onload = function(){
+	lightSpot.image.onload = function(){
 		drawLoop();
 	};
-	lightSpotImage.src = "assets/garden_lightShaftSpot.png";
+	lightSpot.image.src = "assets/garden_lightShaftSpot.png";
 };
 
 //everything happening when mouse is moved
